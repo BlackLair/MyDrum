29a188b73e5f92eb3f661219882e04d056f077e4
JW <kiw970923@naver.com>
2021-03-20 14:28:44 +0900

    타이틀 제거하고 버튼크기 확장

--- app/src/main/java/com/example/mydrum/MainActivity.java
+++ app/src/main/java/com/example/mydrum/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mydrum;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         btn_drum[0]=(Button)findViewById(R.id.kick);
         btn_drum[1]=(Button)findViewById(R.id.snare);
         btn_drum[2]=(Button)findViewById(R.id.hat_open);
